6632bc829b1cdd57fef65e5c505b6d2071dd1dc7
aaronsleepy <aaron@kmong.com>
2022-12-10 22:03:34 +0900

    init: first commit

---
+++ StockRankSwiftUI/StockRankSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockRankSwiftUIApp.swift
+//  StockRankSwiftUI
+//
+//  Created by Aaron on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct StockRankSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
