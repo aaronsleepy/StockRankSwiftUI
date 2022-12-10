b119a3a37c57f0dae3a51777c5cb90bbfe72327c
aaronsleepy <aaron@kmong.com>
2022-12-10 22:12:31 +0900

    assests: stock 이미지 assets 추가

---
+++ StockRankSwiftUI/StockRankRow.swift
@@ -0,0 +1,20 @@
+//
+//  StockRankRow.swift
+//  StockRankSwiftUI
+//
+//  Created by Aaron on 2022/12/10.
+//
+
+import SwiftUI
+
+struct StockRankRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StockRankRow_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankRow()
+    }
+}
