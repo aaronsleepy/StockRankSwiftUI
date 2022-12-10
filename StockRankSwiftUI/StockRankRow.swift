c5edd85704f440c9d3d7ba7bdbf7f1131410d914
aaronsleepy <aaron@kmong.com>
2022-12-10 22:24:05 +0900

    ui: StockRank Row UI 구성 완료

--- StockRankSwiftUI/StockRankRow.swift
+++ StockRankSwiftUI/StockRankRow.swift
@@ -11,19 +11,44 @@ struct StockRankRow: View {
     var body: some View {
         HStack {
             Text("1")
+                .font(.system(size: 16, weight: .bold))
+                .frame(width: 30)
+                .foregroundColor(.blue)
             
             Image("TSLA")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40)
             
-            VStack {
+            VStack(alignment: .leading, spacing: 5) {
                 Text("TSLA")
+                    .font(.system(size: 12))
+                    .foregroundColor(.white)
+                
                 HStack {
                     Text("3,538,631 원")
+                        .font(.system(size: 12))
+                        .foregroundColor(.gray)
                     Text("+0.04%")
+                        .font(.system(size: 12))
+                        .foregroundColor(.red)
                 }
             }
             
+            Spacer()
+            
             Image(systemName: "heart.fill")
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40)
+                .foregroundColor(.gray)
+                
+                
         }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.black)
     }
 }
 
