0883fa6c0da2922aee77808eb2b4581baf0249db
aaronsleepy <aaron@kmong.com>
2022-12-11 20:06:38 +0900

    fix: List의 Favorite 선택에 따른 색상 변경
    - 이런 건 로직 중복인데 viewModel로 옮겨야 하나?

--- StockRankSwiftUI/View/StockRankRow.swift
+++ StockRankSwiftUI/View/StockRankRow.swift
@@ -44,7 +44,7 @@ struct StockRankRow: View {
                 .renderingMode(.template)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40)
-                .foregroundColor(.gray)
+                .foregroundColor(stock.isFavorite ? .white : .gray)
                 
                 
         }
